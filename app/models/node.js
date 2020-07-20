98582f6e1af616824f626b0a5dbe24c32555dd08
Marcelo Mira <hello@marcemira.com>
2020-07-20 01:59:42 -0300

    WIP ember-node-tree integration

--- app/models/node.js
+++ app/models/node.js
@@ -7,6 +7,11 @@ import Model, {
 export default class NodeModel extends Model {
   @attr('string') name
 
+  @attr('boolean', { defaultValue: false }) isSelected
+  @attr('boolean', { defaultValue: false }) isDisabled
+  @attr('boolean', { defaultValue: false }) isExpanded
+  @attr('boolean', { defaultValue: true }) isVisible
+
   @hasMany('node', { inverse: 'parentNode' }) childNodes
   @belongsTo('node', { inverse: 'childNodes' }) parentNode
   @belongsTo('user', { inverse: null }) createdBy
